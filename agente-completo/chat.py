b37c898f590505f91a408eb95edc1b5f102979ae
lpetralli <123559656+lpetralli@users.noreply.github.com>
2024-09-18 10:05:21 -0300

    Update chat.py

--- agente-completo/chat.py
+++ agente-completo/chat.py
@@ -1,28 +1,34 @@
 import streamlit as st
 from agent import Agent
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+import re
+
+prompt = """
+You are a helpful assistant that can answer questions and help with tasks.
+"""
 
 tools = None
 
-prompt = "Be a helpful assistant"
-
 if tools:
-    agent = Agent(model_type="groq", tools=tools, prompt=prompt)
+    agent = Agent(model_type="groq", prompt=prompt, tools=tools)
 else:
     agent = Agent(model_type="groq", prompt=prompt)
 
 
-st.title("Chatbot")
+st.title("Agent Chat Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
 
 # Display all previous chat messages
 for message in st.session_state.messages:
     if isinstance(message, (HumanMessage, AIMessage)) and message.content:
         with st.chat_message("user" if isinstance(message, HumanMessage) else "assistant"):
             st.markdown(message.content)
+
 
 # React to user input
 if prompt := st.chat_input("User input"):
@@ -37,9 +43,12 @@ if prompt := st.chat_input("User input"):
     response_messages = agent.invoke(st.session_state.messages)
 
     # Update the session state with the new response
-    st.session_state.messages.extend(response_messages)
+    st.session_state.messages = response_messages["messages"]
 
     # Display only the last AI message with content
-    last_message = response_messages[-1]
+    last_message = response_messages["messages"][-1]
+
+
     if isinstance(last_message, AIMessage) and last_message.content:
         st.chat_message("assistant").markdown(last_message.content)
+
